19bfe363433a8c8e98f59ce61a9611df118aa151
damsang2000 <damsang163@gmail.com>
2023-06-21 16:31:33 +0700

    deploy update 2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Craete By Sang</h1>
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
